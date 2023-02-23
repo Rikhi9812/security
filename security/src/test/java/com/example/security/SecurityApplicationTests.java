6ec072885cfd2a4e0ff554c23849632c35d698d9
Rikhi9812 <rishi.rana606@gmail.com>
2023-02-23 14:13:43 +0900

    add new edit config

--- security/src/test/java/com/example/security/SecurityApplicationTests.java
+++ security/src/test/java/com/example/security/SecurityApplicationTests.java
@@ -7,13 +7,37 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SecurityApplicationTests {	
 	@Test
 	void contextLoads() {
+		System.out.println(12412412);
 	}
 	@Test
 	void contextLoads1234124() {
+		
+		System.out.println(12412412);
+		System.out.println(12412412);
+		System.out.println(12412412);
+		System.out.println(12412412);
+		System.out.println(12412412);
 	}
 	
 	@Test
 	void contextLoads1234() {
+		System.out.println(12412412);
+		System.out.println(12412412);
+		System.out.println(12412412);
+		System.out.println(12412412);
+		System.out.println(12412412);
+		System.out.println(12412412);
+	}
+	
+	
+	@Test12341234
+	void contextLoads1234() {
+		System.out.println(12412412);
+		System.out.println(12412412);
+		System.out.println(12412412);
+		System.out.println(12412412);
+		System.out.println(12412412);
+		System.out.println(12412412);
 	}
 
 }
