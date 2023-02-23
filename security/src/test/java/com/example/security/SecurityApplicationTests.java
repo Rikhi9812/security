f401d549401017626c8b21387653c4475c52ca30
Rikhi9812 <107738848+Rikhi9812@users.noreply.github.com>
2023-02-23 13:31:27 +0900

    Update SecurityApplicationTests.java

--- security/src/test/java/com/example/security/SecurityApplicationTests.java
+++ security/src/test/java/com/example/security/SecurityApplicationTests.java
@@ -6,7 +6,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SecurityApplicationTests {
 
+	@Test1234123
+	void contextLoads() {
+	}
+	
+	@Testadfa
+	void contextLoads() {
+	}
+	
 	@Test
+	void contextLoads() {
+	}@Test
 	void contextLoads() {
 	}
 
