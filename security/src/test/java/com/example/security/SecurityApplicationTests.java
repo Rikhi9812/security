4521ef85478c505e75d79a14e3cd374d3771e446
Rikhi9812 <107738848+Rikhi9812@users.noreply.github.com>
2023-02-23 15:13:04 +0900

    1

--- security/src/test/java/com/example/security/SecurityApplicationTests.java
+++ security/src/test/java/com/example/security/SecurityApplicationTests.java
@@ -24,18 +24,43 @@ class SecurityApplicationTests {
 	}
 	
 	
+	<<<<<<< HEAD
+	@Test1
+	void contextLoads11() {
+		System.out.println(sdfjkasdkfjan);
+		System.out.println(sdfjkasdkfjan);
+		System.out.println(sdfjkasdkfjan);
+		System.out.println(sdfjkasdkfjan);
+		System.out.println(sdfjkasdkfjan);
+		System.out.println(sdfjkasdkfjan);
+=======
+	asdfasdfasdfas
+	@Testweqwegqwedasdfasdasdfas
+	void contextLoagqwegqwegqwegds1134t1asdfasdasdfa34tasdf() {
+
+	}
+	
 	@Testqwegqwegqasdfasd
 	void contextLoqwegqwegqweads1134t134tasdasdfasdf() {
 
 >>>>>>> refs/remotes/origin/master
 	}
 	
-	@Test2
+	<<<<<<< HEAD
+	@Test1
 	void contextLoads11() {
 		System.out.println(sdfjkasdkfjan);
 		System.out.println(sdfjkasdkfjan);
 		System.out.println(sdfjkasdkfjan);
 		System.out.println(sdfjkasdkfjan);
+		System.out.println(sdfjkasdkfjan);
+		System.out.println(sdfjkasdkfjan);
+=======
+	asdfasdfasdfas
+	@Testweqwegqwedasdfasdasdfas
+	void contextLoagqwegqwegqwegds1134t1asdfasdasdfa34tasdf() {
+
+	});
 		System.out.println(sdfjkasdkfjan);
 	
 		@Test3
@@ -55,7 +80,26 @@ qsdfqwdgqwedgqwedgqwe
 	}
 
 
-
+		ads11() {
+		System.out.println(sdfjkasdkfjan);
+		System.out.println(sdfjkasdkfjan);
+		System.out.asd;ljna:sdkbnva:lksdnvaprintln(sdfjkasdkfjan);
+		System.out.println(sdfjkasdkfjan);
+		System.out.println(sdfjkasdkfjan);
+		System.out.println(sdfjkasdkfjan);
+asdvjan:pdokvna:osdikv
+	sdojvkbna:osikdvn
+		a]pisdkvn:asoidkvn
+			]sidkbv:qoeikjdbv
+				a:osdjvba:osdjibv
+					sodjvb:qsodjbv
+						qdjvbqs:odjbv
+							]qojdvbqsdjv
+							qo:jdvbq:sdjv
+								qs:djvbqs:djbv
+									s:kdjv:qskdjbv
+										qsdjbv:qsdjbv
+											d
 
 	asdfasdfasdfas
 	@Testweqwegqwedasdfasdasdfas
