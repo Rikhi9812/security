450ddf6419b70312eb4ea9f5f6b0220a9bf46bd6
Rikhi9812 <rishi.rana606@gmail.com>
2023-02-23 14:21:06 +0900

    add

--- security/src/test/java/com/example/security/SecurityApplicationTests.java
+++ security/src/test/java/com/example/security/SecurityApplicationTests.java
@@ -6,66 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SecurityApplicationTests {
 
-0
-	@Test12341231
+	@Test
 	void contextLoads() {
 		System.out.println(12412412);
 	}
-	@Test12341232
-	void contextLoads1234124() {
-	}
 	
-	@Test1234123434t123
-	void contextLoads1234() {
-	
-	asfqwerqw
-	}
-	
-	qewrgqwerg
-	ASDGVwdg
-	asdasdfba
-	qerwgq3ewr4g
-	@Test123tqwegqdgq4
-	void contextLoads1234124() {
-	qwegqawergwq
-	}
-	
-	@Testasdfqdfq5
-	void contextLoads1234() {
-	qwegqwegrqwg
-	}
-6
-
-@Test
-	void contextLoads1234124() {
-		
-		System.out.println(12412412);
-		System.out.println(12412412);
-		System.out.println(12412412);
-		System.out.println(12412412);
-		System.out.println(12412412);
-	}
-	
-	@Test
-	void contextLoads1234() {
-		System.out.println(12412412);
-		System.out.println(12412412);
-		System.out.println(12412412);
-		System.out.println(12412412);
-		System.out.println(12412412);
-		System.out.println(12412412);
-	}
-	
-	
-	@Test12341234
-	void contextLoads1234() {
-		System.out.println(12412412);
-		System.out.println(12412412);
-		System.out.println(12412412);
-		System.out.println(12412412);
-		System.out.println(12412412);
-		System.out.println(12412412);
-	}
 
 
 }
