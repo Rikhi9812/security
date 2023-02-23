65adce5ecb2c629539b114c2cfe0ac58bf07e8c2
Rikhi9812 <rishi.rana606@gmail.com>
2023-02-23 15:13:35 +0900

    test

--- security/src/test/java/com/example/security/SecurityApplicationTests.java
+++ security/src/test/java/com/example/security/SecurityApplicationTests.java
@@ -5,129 +5,39 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SecurityApplicationTests {
-
-
-<<<<<<< HEAD
-	@Test1
-	void contextLoads11() {
-		System.out.println(sdfjkasdkfjan);
-		System.out.println(sdfjkasdkfjan);
-		System.out.println(sdfjkasdkfjan);
-		System.out.println(sdfjkasdkfjan);
-		System.out.println(sdfjkasdkfjan);
-		System.out.println(sdfjkasdkfjan);
-=======
-	asdfasdfasdfas
-	@Testweqwegqwedasdfasdasdfas
-	void contextLoagqwegqwegqwegds1134t1asdfasdasdfa34tasdf() {
-
-	}
 	
-	
-	@Testqwegqwegqasdfasd
-	void contextLoqwegqwegqweads1134t134tasdasdfasdf() {
-
->>>>>>> refs/remotes/origin/master
-	}
-	
-	@Test2
-	void contextLoads11() {
-		System.out.println(sdfjkasdkfjan);
-		System.out.println(sdfjkasdkfjan);
-		System.out.println(sdfjkasdkfjan);
-		System.out.println(sdfjkasdkfjan);
-		System.out.println(sdfjkasdkfjan);
-	
-		@Test3
-		void contextLoads11() {
-			System.out.println(sdfjkasdkfjan);
-			System.out.println(sdfjkasdkfjan);
-			System.out.println(sdfjkasdkfjan);
-			System.out.println(sdfjkasdkfjan);
-			System.out.println(sdfjkasdkfjan);
+	@Test
+	void contextText() {
 		
-}
-
-
-@Testqwegqwegasdfasdfasd
-	void qwegqwegqwecontextLoads1asdfasd134t134tasdf() {
-qsdfqwdgqwedgqwedgqwe
 	}
-
-
-
-
-	asdfasdfasdfas
-	@Testweqwegqwedasdfasdasdfas
-	void contextLoagqwegqwegqwegds1134t1asdfasdasdfa34tasdf() {
-
-	}
+	pand:oklvajn
+	[ojasdbv@oasjdb]:[oasjdbv@aosjcdb]asjdbvasjkdcbv
+	kjsdv a;ksj
+	aksjdv a;ksjbv 
+	akscnv a;sknc 
+	as;kjcn va;sk c
+	askcn va;sknc m
+	]
+			
+			
+			
+			
+			
+			dcovj abs;c vjh
+			]ajc ;kajc 
+			akjc ;akn c
+			
+			
+			
+			
+			
+			
+			a:scljb vaskcj 
+			a;ksjcn ;akscn 
+			:akjc ;aksnc 
+			;aknsc ;kas
 	
-	
-	@Testqwegqwegqasdfasd
-	void contextLoqwegqwegqweads1134t134tasdasdfasdf() {
-
-	}
 }
 
 
-@Testqwegqwegasdfasdfasd
-	void qwegqwegqwecontextLoads1asdfasd134t134tasdf() {
-qsdfqwdgqwedgqwedgqwe
-	}
-}
-}]
-
-
-package com.example.security;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SecurityApplicationTests {
-
-
-	asdfasdfasdfas
-	@Testweqwegqwedasdfasdasdfas
-	void contextLoagqwegqwegqwegds1134t1asdfasdasdfa34tasdf() {
-
-	}
-	
-	
-	@Testqwegqwegqasdfasd
-	void contextLoqwegqwegqweads1134t134tasdasdfasdf() {
-
-	}
-}
-
-
-@Testqwegqwegasdfasdfasd
-	void qwegqwegqwecontextLoads1asdfasd134t134tasdf() {
-qsdfqwdgqwedgqwedgqwe
-	}
-
-
-
-
-	asdfasdfasdfas
-	@Testweqwegqwedasdfasdasdfas
-	void contextLoagqwegqwegqwegds1134t1asdfasdasdfa34tasdf() {
-
-	}
-	
-	
-	@Testqwegqwegqasdfasd
-	void contextLoqwegqwegqweads1134t134tasdasdfasdf() {
-
-	}
-}
-
-
-@Testqwegqwegasdfasdfasd
-	void qwegqwegqwecontextLoads1asdfasd134t134tasdf() {
-qsdfqwdgqwedgqwedgqwe
-	}
-}
-}
 
