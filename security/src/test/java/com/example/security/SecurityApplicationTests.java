897c5089fab09790f647c59b8880bc97df992d2a
Rikhi9812 <107738848+Rikhi9812@users.noreply.github.com>
2023-02-23 14:22:13 +0900

    Update SecurityApplicationTests.java
    12345

--- security/src/test/java/com/example/security/SecurityApplicationTests.java
+++ security/src/test/java/com/example/security/SecurityApplicationTests.java
@@ -6,8 +6,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SecurityApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Test1
+	void contextLoads1() {
+		System.out.println(12412412);
+	}
+	@Test2
+	void contextLoads2() {
+		System.out.println(12412412);
+	}
+	@Test3
+	void contextLoads3() {
+		System.out.println(12412412);
+	}
+	@Test4
+	void contextLoads4() {
+		System.out.println(12412412);
+	}
+	@Test5
+	void contextLoads4() {
 		System.out.println(12412412);
 	}
 	
